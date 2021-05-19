0c0b297ef7b7c9818e9fd1f7036326f1e239ad05
Chris Castellano <c.castellano18@gmail.com>
2021-05-19 14:23:29 -0500

    fixes

--- Base_app/client/src/CommentList.js
+++ Base_app/client/src/CommentList.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
 
-export default ({comments}) => {
- const renderedComments = comments.map(comment => {
-  let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
 
-  if(comment.status === 'approved'){
-    content = comment.content
-  }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
 
-  if(comment.status === 'pending'){
-    content = 'comment is pending'
-  }
-  if(comment.status === 'rejected'){
-    content = 'this comment is rejected'
-  }
-   return <li key={comment.id}>{content}</li>
- })
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
 
-  return <ul>
-    {renderedComments}
-  </ul>
-}
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
